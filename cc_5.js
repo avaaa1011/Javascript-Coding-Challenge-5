5e85bdacf3f15056662493d4c277895b7515b7b0
Ava Tran <ava31@usf.edu>
2025-02-09 12:24:59 -0500

    Task 1: Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,11 @@
+//Task 1 - Object Properties
+let customer = //declaring the customer profile with multiple properties
+    {
+        name: "John Doe",
+        age: 35,
+        email: "john.doe@email.com"
+    }; 
+    console.log(`Customer's Name: ${customer.name}`); // printing customer name
+    console.log(`Customer's Age: ${customer.age}`); // printing customer age
+    console.log(`Customer's Email: ${customer.email}`); // printing customer email
+    
