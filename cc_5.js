8da86c6b8a5785e2fc37d15901ca8f8f787a772f
Ava Tran <ava31@usf.edu>
2025-02-09 13:20:58 -0500

    Task 3: Shoppinh Cart

--- cc_5.js
+++ cc_5.js
@@ -19,3 +19,13 @@ let order = //declaring an object with multiple properties
         {return console.log(`Order ID: ${this.orderId} | Total Amount: $${this.totalAmount} | Status: ${this.status}`)}
     }
 order.displayOrder(); //calling the method to log the details
+
+//Task 3 - Shopping Cart
+let cartItems = ["Pasta", "Milk", "Sauce"]; //declaring an array
+console.log(`Initial Cart Items: ${cartItems}`); // Lists the first few items
+cartItems.push("Chicken"); // Adding another item using push()
+cartItems.pop(); //removing the last item in the array
+cartItems.unshift("Noodles"); //adding another item to the beggining of the array
+console.log(`Final Cart Items: ${cartItems}`); //prints updated list of cartItems
+
+
